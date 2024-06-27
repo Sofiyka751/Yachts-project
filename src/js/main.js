95d4617dbab036c7ba6d78c7f8fe09bbd4e48cde
Sofiyka751 <kolozay.sofiya@gmail.com>
2024-06-27 11:38:45 +0300

    fourth

--- src/js/main.js
+++ src/js/main.js
@@ -31,3 +31,53 @@ const mySlider = new Splide('#mySlider',{
 })
 
 mySlider.mount()
+
+document.addEventListener("DOMContentLoaded", function (){
+  const burgerBtn = document.getElementById("burger");
+  const menu = document.getElementById("menu");
+  const header = document.querySelector("header");
+
+  burgerBtn.addEventListener("click", function(){
+      header.classList.toggle("open");
+      menu.classList.toggle("open");
+  });
+});
+
+var swiper = new Swiper('.swiper', {
+slidesPerView: getSlidesPerView(),
+direction: getDirection(),
+navigation: {
+  nextEl: '.swiper-button-next',
+  prevEl: '.swiper-button-prev',
+},
+on: {
+  resize: function () {
+    swiper.changeDirection(getDirection());
+    swiper.params.slidesPerView = getSlidesPerView();
+    swiper.update();
+  },
+},
+});
+
+function getDirection() {
+var windowWidth = window.innerWidth;
+return windowWidth <= 115 ? 'vertical' : 'horizontal';
+}
+
+function getSlidesPerView() {
+var windowWidth = window.innerWidth;
+if (windowWidth >= 1280) {
+  return 3;
+} else if (windowWidth >= 768) {
+  return 2;
+} else {
+  return 1;
+}
+}
+
+VanillaTilt.init(document.querySelectorAll('.yachts-block__card-set'), {
+max: 3,
+speed: 400,
+glare: true,
+'max-glare': 0.1,
+});
